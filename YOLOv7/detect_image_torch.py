f01e72e30bdb053367d52d700b43d489ff52f815
JinFree <ytrqwe12@gmail.com>
2023-05-29 21:01:32 +0900

    yolov7 torch script

--- YOLOv7/detect_image_torch.py
+++ YOLOv7/detect_image_torch.py
@@ -1,19 +1,17 @@
+## NEED TO MOVE THIS SCRIPT TO YOLOV7 PATH
 import argparse
 import time
 from pathlib import Path
-
 import cv2
 import torch
 import torch.backends.cudnn as cudnn
 from numpy import random
 import numpy as np
-
 from models.experimental import attempt_load
-from utils.datasets import LoadStreams, LoadImages, letterbox
-from utils.general import check_img_size, check_requirements, check_imshow, non_max_suppression, apply_classifier, \
-    scale_coords, xyxy2xywh, strip_optimizer, set_logging, increment_path
+from utils.datasets import letterbox
+from utils.general import check_img_size, non_max_suppression, scale_coords, set_logging, increment_path
 from utils.plots import plot_one_box
-from utils.torch_utils import select_device, load_classifier, time_synchronized, TracedModel
+from utils.torch_utils import select_device, time_synchronized, TracedModel
 
 
 class DetectClass():
